a90706a839221f4b5606927416904de0bdf5788d
kamangir <arash@kamangir.net>
2025-02-19 21:39:16 -0800

    start of cli-refactors-2025-02-19-wmodaw 🪄 - kamangir/bolt#746

---
+++ google_earth_as_gltf/__init__.py
@@ -0,0 +1,17 @@
+NAME = "google_earth_as_gltf"
+
+ICON = "🌀"
+
+DESCRIPTION = f"{ICON} Google Earth as glTF."
+
+VERSION = "5.2.1"
+
+REPO_NAME = "google-earth-as-gltf"
+
+MARQUEE = "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true"
+
+ALIAS = "@gltf"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
